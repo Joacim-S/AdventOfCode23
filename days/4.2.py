6f51859b314769663b66ddea1ace6833211a3f6c
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2023-12-06 00:04:16 +0200

    Start 4.2

--- days/4.2.py
+++ days/4.2.py
@@ -1,15 +1,29 @@
 from data_reader import read_data
 
 def calculate(data):
+    games = {}
     for row in data:
         if not row:
             break
         row = row.split(':')
         game = int(row[0][5:])
-        print(game)
         numbers = row[1].split('|')
-        winners = set(filter(lambda number: number, numbers[0].split(' ')))
-        print(winners)
+        winners = get_numbers(numbers[0])
+        my_numbers = get_numbers(numbers[1])
+        wins = get_wins(my_numbers, winners)
+        print(wins)
+        
+def get_wins(numbers, winners):
+    wins = 0
+    for n in numbers:
+        if n in winners:
+            wins += 1
+    
+    return wins
+
+def get_numbers(number_string):
+    numbers = set(filter(lambda number: number, number_string.split(' ')))
+    return numbers
 
 if __name__ == '__main__':
-    print(calculate(read_data('4.txt')))
+    print(calculate(read_data('4test.txt')))
